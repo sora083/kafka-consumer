632907bf7fb5b21eb6aae7fd6d630d6741414f4d
Sora Tomita <sora.tomita083@gmail.com>
2018-07-17 23:04:40 +0900

    change consumer

--- src/main/java/com/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
+++ src/main/java/com/kafka/consumer/kafkaconsumer/KafkaConsumerApplication.java
@@ -1,14 +1,31 @@
 package com.kafka.consumer.kafkaconsumer;
 
+import com.kafka.consumer.kafkaconsumer.consumer.SimpleConsoleConsumer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
 
+@Slf4j
 @SpringBootApplication
-@EnableKafka
+//@EnableKafka
 public class KafkaConsumerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaConsumerApplication.class, args);
 	}
+//
+//	@Override
+//	public void run(String... args) throws Exception {
+//		log.info("All received");
+//	}
+//
+//	@KafkaListener(id = "twitter-consume", topics = "tweets")
+//	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+//		log.info(cr.toString());
+//		//latch.countDown();
+//	}
 }
--- src/main/java/com/kafka/consumer/kafkaconsumer/consumer/SimpleConsoleConsumer.java
+++ src/main/java/com/kafka/consumer/kafkaconsumer/consumer/SimpleConsoleConsumer.java
@@ -1,57 +1,35 @@
 package com.kafka.consumer.kafkaconsumer.consumer;
 
 import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Properties;
 
+@Slf4j
+@Component
+@EnableKafka
 public class SimpleConsoleConsumer {
-    public static void main(String... args) {
-        SimpleConsoleConsumer consoleConsumer = new SimpleConsoleConsumer();
-        consoleConsumer.consuming();
-    }
 
-    public void consuming() {
+    @KafkaListener(id = "twitter-consume", topics = "tweets")
+    public void listen() {
         Properties properties = new Properties();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "twitter-consume");
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.0.14:9092");
+        //properties.put(ConsumerConfig.GROUP_ID_CONFIG, "twitter-consume");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         //properties.put("jackson.json.deserializer.type", LinkedHashMap.class);
 
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties)) {
             consumer.subscribe(Arrays.asList("tweets"));
-
-            Set<String> collectIds = new TreeSet<>();
-
-            long startTime = System.nanoTime();
-
-            while (true) {
-                ConsumerRecords<String, String> records = consumer.poll(1000L);
-
-                records.forEach(record -> {
-                    String value = record.value();
-
-                    System.out.println("=== received record: ");
-                    System.out.println(value);
-                });
-
-                long elapsedTime = System.nanoTime() - startTime;
-
-                if (TimeUnit.MINUTES.convert(elapsedTime, TimeUnit.NANOSECONDS) >= 1L) {
-                    break;
-                }
-            }
-
-            System.out.println();
-            System.out.println("===== collected time / id");
-            collectIds.forEach(System.out::println);
+            ConsumerRecords<String, String> records = consumer.poll(1000L);
+            records.forEach(r -> log.info(r.key() + ":" + r.value()));
         }
     }
 }
